19343943da8a77fda0f00f5419268cd7d1315f48
GreyWarden <raulpavonfp@gmail.com>
2022-12-30 22:45:43 +0100

    now storybook loads up

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,7 +8,16 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    {
+      name: 'storybook-addon-sass-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+        loadSassAfterPostCSS: true,
+      },
+    },
   ],
   "framework": "@storybook/react",
   "core": {
