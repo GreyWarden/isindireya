1fa67705d712bb5044a3cb6cb19a9e96c34c225c
GreyWarden <raulpavonfp@gmail.com>
2023-02-24 11:17:11 +0100

    Remove test component

--- modules/HelloThere.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react'
-import styles from '@modules/HelloThere.module.scss'
-
-export const HelloThere: FC = () => (
-    <div className={ styles.helloThere__container }>
-        Hello there!
-    </div>
-)
