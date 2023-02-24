420acfdcac95ddfaf5787897532c5c1821176e9e
GreyWarden <raulpavonfp@gmail.com>
2023-02-24 15:58:44 +0100

    Change CharacterNodeCreator to FC

--- modules/CharacterNodeCreator/CharacterNodeCreator.tsx
+++ modules/CharacterNodeCreator/CharacterNodeCreator.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react'
+import { FC, useState } from 'react'
 import styles from './CharacterNodeCreator.module.scss'
 
 interface Character {
@@ -8,32 +8,15 @@ interface Character {
   description: string
 }
 
-interface State {
-  characters: Character[]
-}
+export const CharacterNodeCreator: FC = () => {
+  const [characters, setCharacters] = useState<Character[]>(() => [{
+    name: 'Aye! Aye!',
+    position: 'Capitán',
+    gender: 'nb',
+    description: 'Quién vive en la piña debajo del mar?',
+  }])
 
-export class CharacterNodeCreator extends Component {
-  state: State = {
-    characters: [],
-  }
-
-  componentDidMount () {
-    this.setState((state: State) => ({
-      characters: [
-        ...state.characters,
-        {
-          name: 'Aye! Aye!',
-          position: 'Capitán',
-          gender: 'nb',
-          description: 'Quién vive en la piña debajo del mar?',
-        },
-      ],
-    }))
-  }
-
-  render () {
-    return (<div className={ styles['character-node-creator__container'] } >
-      { this.state.characters.map((character) => (character.name)) }
-    </div>)
-  }
+  return (<div className={ styles['character-node-creator__container'] } >
+    { characters.map((character) => (character.name)) }
+  </div>)
 }
