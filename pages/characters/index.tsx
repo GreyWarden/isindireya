310219fe5ecfd0d5c0c7132bf61565d95f8d7b81
GreyWarden <raulpavonfp@gmail.com>
2023-04-22 12:20:56 +0200

    Moved CharacterPage to its own directory

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -1,17 +1,3 @@
-import Head from 'next/head'
-import {
-  NodeCreator as CharacterNodeCreator
-} from '@modules/Character/Infrastructure/Components/NodeCreator/NodeCreator'
+import { CharacterPage } from '@modules/Character/Infrastructure/Pages/CharacterPage'
 
-export default function Home () {
-  return (
-        <>
-            <Head>
-                <title>Isindireya&apos;s Character Creation</title>
-            </Head>
-            <div>
-                <CharacterNodeCreator />
-            </div>
-        </>
-  )
-}
+export default CharacterPage
