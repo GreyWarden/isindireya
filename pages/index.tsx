8dc8bcb86d411873bbd767ffb9dd62b399141a1e
GreyWarden <raulpavonfp@gmail.com>
2023-04-22 12:14:02 +0200

    Moved HomePage to its own directory

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,3 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import { HomePage } from '@modules/Home/Infrastructure/Pages/HomePage'
 
-export default function Home () {
-  return (
-    <>
-      <Head>
-        <title>Isindireya</title>
-      </Head>
-      <section>
-          <h2>Welcome to Isindireya</h2>
-          <p>
-              Isindireya is designed to be a tool for your RPG world building.
-              In it, you can create characters, places, events and set relations between them.
-          </p>
-          <p>Down below you will find links to each creation tool.</p>
-          <div>
-              <Link href={ '/characters' }> Character creator </Link>
-          </div>
-      </section>
-    </>
-  )
-}
+export default HomePage
